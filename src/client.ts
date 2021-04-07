918b2b8b725869bebeb95f6ac8607e50645eb26e
Marvin Witt <nurmarvin@protonmail.com>
2021-04-07 17:36:56 +0200

    fix: Ban & Unban functions now work

--- src/client.ts
+++ src/client.ts
@@ -231,7 +231,7 @@ export default class Client extends EventEmitter {
    * @param uuid The UUID (with `-`) of the user to ban.
    */
   banUser(uuid: string) {
-    this.sendPacket('BanUser', { uuid });
+    this.sendPacket('BanUser', { user: uuid });
   }
 
   /**
@@ -241,7 +241,7 @@ export default class Client extends EventEmitter {
    * @param uuid The UUID (with `-`) of the user to unban.
    */
   unbanUser(uuid: string) {
-    this.sendPacket('UnbanUser', { uuid });
+    this.sendPacket('UnbanUser', { user: uuid });
   }
 
   /**
